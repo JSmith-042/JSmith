e891d6d255247a2ccd753852c737b2a606211e14
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-03-17 10:39:52 -0500

    add menu functionality in js

---
+++ HTML JAVASCRIPT CSS/labs/Lab4/Menu.js
@@ -0,0 +1,4 @@
+let menuItems = [{name: "Porky", description: "Pork Pot Pie", price: "15.45"}, {name: "Beefy", description: "Beef Pot Pie", price: "19.99"},
+                                            {name: "Cluck Cluck", description: "Chicken Pot Pie", price: "16.75"}, {name: "Glizzy", description: "Hot Dog With Bun", price:"3.75"},
+                                            {name: "Triple Stacks", description: "Three Pancakes With Toppings", price: "8.99"}];
+
