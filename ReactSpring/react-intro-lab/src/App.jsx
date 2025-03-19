cb385a6eb8ee3aad1a80ccf7cc0e4ef378ae38c4
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-03-19 11:15:40 -0500

    lab1 part1 complete

--- ReactSpring/react-intro-lab/src/App.jsx
+++ ReactSpring/react-intro-lab/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import "./components/Greeting.jsx"
+import {Greeting} from "./components/Greeting.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <div>
+        <Greeting />
+      </div>
     </>
   )
 }
