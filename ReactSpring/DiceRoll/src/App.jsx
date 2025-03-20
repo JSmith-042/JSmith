005c3c459a9edba54a5a133ca529a7caca642bb0
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-03-20 09:33:38 -0500

    initial setup

---
+++ ReactSpring/DiceRoll/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import {genRanNum} from "./helper.js";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ ReactSpring/DiceRoll/src/helper.js
@@ -0,0 +1,4 @@
+export function genRanNum()
+{
+    return Math.floor(Math.random()*6).toString();
+}
