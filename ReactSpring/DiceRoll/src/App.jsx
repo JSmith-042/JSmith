233fc9acada8fd1191fac7e63e491957759c4df9
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-03-20 12:04:05 -0500

    lab2 complete

--- ReactSpring/DiceRoll/src/App.jsx
+++ ReactSpring/DiceRoll/src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './styles/App.css'
 import {genRanNum} from "./helper.js";
 import Dice from "./components/Dice.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [die1, setDie1] = useState(genRanNum());
+  const [die2, setDie2] = useState(genRanNum());
+
+  const [total, setTotal] = useState(die1+die2 + 2);
+
+  const rollClick = () => {setDie1(genRanNum);setDie2(genRanNum);};
+
+    useEffect(() => {
+        setTotal(die1+die2 + 2)
+    }, [die1, die2]);
 
   return (
       <div>
           <p>Roll the dice if you dare!</p>
-          <Dice num={genRanNum()}></Dice>
-          <Dice num={genRanNum()}></Dice>
-
+          <Dice num={die1}></Dice>
+          <Dice num={die2}></Dice>
+          <div style={{padding:0}}>
+            <button onClick={rollClick}>Roll</button>
+          </div>
+          <div>
+              You rolled: {total}
+          </div>
       </div>
   )
 }
--- ReactSpring/DiceRoll/src/components/Dice.jsx
+++ ReactSpring/DiceRoll/src/components/Dice.jsx
@@ -1,8 +1,10 @@
 import {diceNums} from "../helper.js";
 import '../styles/Dice.css';
+import {useState} from "react";
 
 export default function Dice(props)
 {
+    const [number, setNum] = useState(props.num);
 
     return (<div className={"fas fa-dice-" + diceNums[props.num]}></div>);
 }
