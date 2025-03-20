de488ca8280d63a6dc9577d90e9528644e8e48bc
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-03-20 11:57:53 -0500

    update helper

--- ReactSpring/DiceRoll/src/helper.js
+++ ReactSpring/DiceRoll/src/helper.js
@@ -1,6 +1,6 @@
 export function genRanNum()
 {
-    return Math.floor(Math.random()*6).toString();
+    return Math.floor(Math.random()*6);
 }
 
 export const diceNums = ["one", "two", "three", "four", "five", "six"];
