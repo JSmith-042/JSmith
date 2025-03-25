0fddaef6d81248bb80c4d7cda6ca8722ab133490
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-03-25 10:07:14 -0500

    refactor props

--- ReactSpring/DiceRoll/src/components/Dice.jsx
+++ ReactSpring/DiceRoll/src/components/Dice.jsx
@@ -2,9 +2,9 @@ import {diceNums} from "../helper.js";
 import '../styles/Dice.css';
 import {useState} from "react";
 
-export default function Dice(props)
+export default function Dice({num})
 {
-    const [number, setNum] = useState(props.num);
+    const [number, setNum] = useState(num);
 
-    return (<div className={"fas fa-dice-" + diceNums[props.num]}></div>);
+    return (<div className={"fas fa-dice-" + diceNums[num]}></div>);
 }
