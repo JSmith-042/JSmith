b2bd896aaa2187a06cc918aad961f8ac9edd7afd
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-05 09:42:58 -0500

    setup basic theme component

--- ReactSpring/TMDB/src/components/ui/Theme.jsx
+++ ReactSpring/TMDB/src/components/ui/Theme.jsx
@@ -1,4 +1,22 @@
+import { createTheme } from '@mui/system';
+
 export default function Theme()
 {
-
+    return createTheme({
+        palette: {
+            background: {
+                paper: '#fff',
+            },
+            text: {
+                primary: '#173A5E',
+                secondary: '#46505A',
+            },
+            action: {
+                active: '#001E3C',
+            },
+            success: {
+                dark: '#009688',
+            },
+        },
+    });
 }
