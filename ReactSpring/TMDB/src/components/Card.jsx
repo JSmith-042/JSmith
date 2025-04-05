48f0f6e79271c99bf4c5e92d974ba96a1c6817a9
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-05 09:42:11 -0500

    refactor images location

--- ReactSpring/TMDB/src/components/Card.jsx
+++ ReactSpring/TMDB/src/components/Card.jsx
@@ -3,11 +3,18 @@ import {Paper} from "@mui/material";
 export default function Card({movie})
 {
     const {title, overview, vote_average, poster_path} = movie;
-    const posterImage = `https://image.tmdb.org/t/p/w185${poster_path}`;
+
+    let posterImage;
+
+    if (poster_path == null)
+        posterImage = "/assets/notFound.gif";
+    else
+        posterImage = `https://image.tmdb.org/t/p/w185${poster_path}`;
+
 
     return <Paper elevation={16} style={{minHeight: "500px"}}>
         <h1>{title}</h1>
-        <img src={posterImage} alt=""/>
+        <img src={posterImage} alt={title}/>
         <p>{overview}</p>
         <p>{vote_average}</p>
     </Paper>
