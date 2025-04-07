5734625fba9fe3824c1ba80f66f9e9ed9a1c75c2
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-07 12:17:39 -0500

    update styles

--- ReactSpring/TMDB/src/components/Card.jsx
+++ ReactSpring/TMDB/src/components/Card.jsx
@@ -13,10 +13,10 @@ export default function Card({movie})
         posterImage = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
 
-    return  <Paper elevation={16} style={{height: "50vw", textAlign: "center", borderRadius: "30px", padding:10}}>
+    return  <Paper elevation={16} style={{height: "40vw", textAlign: "center", placeItems: "center", borderRadius: "30px", padding:10, display:"flex", flexDirection:"column"}}>
         <Typography sx={{fontSize: "2vw"}} color={"secondary"}>{title}</Typography>
         <img src={posterImage} alt={title} style={{width:"50%"}}/>
-        <Typography sx={{fontSize: "1.3vw", overscrollBehavior: "auto", scrollBehavior:"smooth"}}>{overview}</Typography>
-        <Typography variant={"h6"} sx={{fontSize: "2vw"}}>{vote_average}</Typography>
+        <Typography sx={{fontSize: "1.3vw", flexGrow: 1, overflow:"scroll"}}>{overview}</Typography>
+        <Typography variant={"h6"} sx={{fontSize: "1.5vw"}}>{vote_average}</Typography>
     </Paper>
 }
