96efc8f614b20e85935d373db3e755cab0569135
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-14 10:55:09 -0500

    add data

---
+++ ReactSpring/BucketLists/FullStackBucketDemo/server/mockData.js
@@ -0,0 +1,17 @@
+exports.bucketlist = [
+  {
+    id: 1,
+    description: "This is the <b>First</b> item",
+    isComplete: "false"
+  },
+  {
+    id: 2,
+    description: "This is the <b>Second</b> item",
+    isComplete: "false"
+  },
+  {
+    id: 3,
+    description: "This is the <b>Third</b> item",
+    isComplete: "true"
+  }
+]
